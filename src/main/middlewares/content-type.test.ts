31807f848337673fc2ce3a64d989dc137390a7f0
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-04 01:00:53 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('ContentType', () => {
+  it('should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      return res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  it('should return xml type content when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      return res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
