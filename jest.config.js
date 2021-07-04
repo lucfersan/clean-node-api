fe7302b9e0d50a51ce32ef15558c93197bb1c340
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-04 03:23:34 -0300

    refactor: change jest coverage provider and remove main from collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
