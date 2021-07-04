d7ec3bcee3e30cf685c0a48e7152a6080dd24949
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-04 00:49:29 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Cors', () => {
+  it('should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      return res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .send()
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
