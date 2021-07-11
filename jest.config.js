a6fa804f151e7d23ad592e5d8bd00ecee3d3ba27
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-11 02:48:37 -0300

    refactor: protocols

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/protocols/*.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
