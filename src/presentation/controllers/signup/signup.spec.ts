656cb068d7db59d493866cdbc11da222f444cab2
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-11 02:06:32 -0300

    feat: ensure SignUpController returns 400 if Validation returns an error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -226,4 +226,14 @@ describe('SignUpController', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toBeCalledWith(httpRequest.body)
   })
+
+  it('should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
