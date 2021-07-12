13134b2390ab8f78f1ec310912d1b4fe1b3c40d0
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-12 03:55:44 -0300

    refactor: change bcryptjs to bcrypt

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 import { Encrypter } from '../../data/protocols/cryptography/encrypter'
 
 export class BcryptAdapter implements Encrypter {
