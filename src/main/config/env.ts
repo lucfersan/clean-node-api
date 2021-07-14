298ea8df0f8015fec4ddf65b3c3764eee6a5805a
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-14 00:57:01 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,7 @@
-export default {
+const env = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 3333
+  port: process.env.PORT || 3333,
+  jwtSecret: process.env.JWT_SECRET || 'secret'
 }
+
+export default env
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,7 +2,7 @@ import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
 import { resolve } from 'path'
 
-export default (app: Express): void => {
+export default function setupRoutes(app: Express): void {
   const router = Router()
   app.use('/api', router)
   readdirSync(resolve(__dirname, '..', 'routes')).map(async file => {
