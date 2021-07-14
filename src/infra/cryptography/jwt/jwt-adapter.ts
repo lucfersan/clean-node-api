44e97e54d9b7a91841eb4ebe47be219464c62761
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-14 01:23:23 -0300

    feat: add expires in to JwtAdapter

--- src/infra/cryptography/jwt/jwt-adapter.ts
+++ src/infra/cryptography/jwt/jwt-adapter.ts
@@ -5,6 +5,8 @@ export class JwtAdapter implements Encrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(value: string): Promise<string> {
-    return jwt.sign({ id: value }, this.secret)
+    return jwt.sign({ id: value }, this.secret, {
+      expiresIn: '1d'
+    })
   }
 }
