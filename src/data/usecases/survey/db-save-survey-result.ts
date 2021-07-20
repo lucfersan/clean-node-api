fda9dc438bf5e6234c69f18731b6f41bbb5f7220
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-20 05:29:52 -0300

    feat: ensure DbSaveSurveyResult calls DbSaveSurveyResult with correct values

---
+++ src/data/usecases/survey/db-save-survey-result.ts
@@ -0,0 +1,14 @@
+import { SaveSurveyResultRepository } from '@/data/protocols'
+import { SurveyResultModel } from '@/domain/models'
+import { SaveSurveyResult, SaveSurveyResultModel } from '@/domain/usecases'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor(
+    private readonly saveSurveyResultRepository: SaveSurveyResultRepository
+  ) {}
+
+  async save(surveyData: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    await this.saveSurveyResultRepository.save(surveyData)
+    return null
+  }
+}
