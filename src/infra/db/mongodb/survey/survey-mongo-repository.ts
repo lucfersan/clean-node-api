acbd878b7cceec32d69d28d0d4870b11a765f7eb
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-20 05:50:40 -0300

    feat: ensure SurveyMongoRepository load survey by id

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -2,12 +2,16 @@ import {
   AddSurveyRepository,
   DataAddSurveyModel,
   DataSurveyModel,
+  LoadSurveyByIdRepository,
   LoadSurveysRepository
 } from '@/data/protocols'
 import { MongoHelper } from '@/infra/db'
 
 export class SurveyMongoRepository
-  implements AddSurveyRepository, LoadSurveysRepository
+  implements
+    AddSurveyRepository,
+    LoadSurveysRepository,
+    LoadSurveyByIdRepository
 {
   async add(surveyData: DataAddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
@@ -16,7 +20,13 @@ export class SurveyMongoRepository
 
   async loadSurveys(): Promise<DataSurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const surveys: DataSurveyModel[] = await surveyCollection.find().toArray()
+    const surveys = await surveyCollection.find().toArray()
     return MongoHelper.mapCollection(surveys)
+  }
+
+  async loadById(id: string): Promise<DataSurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: id })
+    return MongoHelper.map(survey)
   }
 }
