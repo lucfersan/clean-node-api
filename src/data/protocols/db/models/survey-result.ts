60678590243db721634e838f0221cf05d5d93ad7
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-20 05:40:41 -0300

    feat: add survey result models to data layer

---
+++ src/data/protocols/db/models/survey-result.ts
@@ -0,0 +1,5 @@
+import { SurveyResultModel } from '@/domain/models'
+import { SaveSurveyResultModel } from '@/domain/usecases'
+
+export type DataSurveyResultModel = SurveyResultModel
+export type DataSaveSurveyResultModel = SaveSurveyResultModel
