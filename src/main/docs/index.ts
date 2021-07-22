47296debdcb1057e745fc9eeb602fccb38fc8365
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-22 11:55:20 -0300

    feat: add swagger api config

---
+++ src/main/docs/index.ts
@@ -0,0 +1,11 @@
+const configDocs = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description:
+      'Api para realizar enquete entre programadores desenvolvida no curso do Rodrigo Manguinho',
+    version: '1.9.0'
+  }
+}
+
+export default configDocs
