6598c2c11c175135500e4f2505ab15cb70d179a1
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-07-30 23:50:29 -0300

    test: ensure SurveyMongoRepository loadSurveys using a account id

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,6 +1,7 @@
 import { Collection } from 'mongodb'
 
-import { mockAddSurveyParams } from '@/domain/test'
+import { DataAccountModel } from '@/data/protocols'
+import { mockAddAccountParams, mockAddSurveyParams } from '@/domain/test'
 import { MongoHelper } from '@/infra/db'
 
 import { SurveyMongoRepository } from './survey-mongo-repository'
@@ -10,6 +11,14 @@ const makeSut = (): SurveyMongoRepository => {
 }
 
 let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const mockAccount = async (): Promise<DataAccountModel> => {
+  const res = await accountCollection.insertOne(mockAddAccountParams())
+  return MongoHelper.map(res.ops[0])
+}
+
 describe('SurveyMongoRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -18,6 +27,10 @@ describe('SurveyMongoRepository', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   afterAll(async () => {
@@ -36,20 +49,31 @@ describe('SurveyMongoRepository', () => {
 
   describe('loadSurveys()', () => {
     it('should load surveys', async () => {
+      const account = await mockAccount()
       const addSurveyModels = [mockAddSurveyParams(), mockAddSurveyParams()]
-      await surveyCollection.insertMany(addSurveyModels)
+      const result = await surveyCollection.insertMany(addSurveyModels)
+      const survey = MongoHelper.map(result.ops[0])
+      await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
       const sut = makeSut()
-      const surveys = await sut.loadSurveys()
+      const surveys = await sut.loadSurveys(account.id)
       expect(surveys.length).toBe(2)
       expect(surveys[0].id).toBeTruthy()
       expect(surveys[0].question).toBe(addSurveyModels[0].question)
+      expect(surveys[0].didAnswer).toBe(true)
       expect(surveys[1].id).toBeTruthy()
       expect(surveys[1].question).toBe(addSurveyModels[1].question)
+      expect(surveys[1].didAnswer).toBe(false)
     })
 
     it('should return an empty array', async () => {
+      const account = await mockAccount()
       const sut = makeSut()
-      const surveys = await sut.loadSurveys()
+      const surveys = await sut.loadSurveys(account.id)
       expect(surveys.length).toBe(0)
     })
   })
