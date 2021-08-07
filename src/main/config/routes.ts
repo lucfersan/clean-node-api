7587d1cbfecaeb3ecb2b8f75bd338569564493d2
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-07 17:20:24 -0300

    refactor: remove unused expression in routes config

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export default function setupRoutes(app: Express): void {
   const router = Router()
   app.use('/api', router)
   readdirSync(resolve(__dirname, '../routes')).map(async file => {
-    if (!file.endsWith('.test.ts') && !file.endsWith('.map')) {
+    if (!file.endsWith('.map')) {
       const route = await import(resolve(__dirname, '../routes', file))
       route.default(router)
     }
