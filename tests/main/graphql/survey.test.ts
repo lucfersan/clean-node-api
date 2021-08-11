42b55320a34254ec810afa88c324889b5c410104
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-08-10 23:26:55 -0300

    test: ensure surveys query returns surveys on success

--- tests/main/graphql/survey.test.ts
+++ tests/main/graphql/survey.test.ts
@@ -1,116 +1,113 @@
-it('should sum two numbers', () => {
-  expect(1 + 1).toBe(2)
+import { ApolloServer, gql } from 'apollo-server-express'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
+
+import { MongoHelper } from '@/infra/db'
+import env from '@/main/config/env'
+
+import { makeApolloServer } from './helpers'
+
+const mockAccessToken = async (): Promise<string> => {
+  const password = await hash('any_password', 12)
+  const result = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password,
+    role: 'admin'
+  })
+  const id = result.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret, {
+    expiresIn: '1d'
+  })
+  await accountCollection.updateOne(
+    { _id: id },
+    {
+      $set: {
+        accessToken
+      }
+    }
+  )
+  return accessToken
+}
+
+let accountCollection: Collection
+let surveyCollection: Collection
+let apolloServer: ApolloServer
+describe('Survey GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('Surveys Query', () => {
+    const surveysQuery = gql`
+      query surveys {
+        surveys {
+          id
+          question
+          answers {
+            image
+            answer
+          }
+          date
+          didAnswer
+        }
+      }
+    `
+    it('should return surveys', async () => {
+      const accessToken = await mockAccessToken()
+      const now = new Date()
+      await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          },
+          {
+            answer: 'other_answer'
+          }
+        ],
+        date: now
+      })
+      apolloServer.requestOptions.context = {
+        req: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      }
+      const res = await apolloServer.executeOperation({
+        query: surveysQuery
+      })
+      console.log(res.errors?.[0])
+      expect(res.data.surveys.length).toBe(1)
+      expect(res.data.surveys[0].id).toBeTruthy()
+      expect(res.data.surveys[0].question).toBe('any_question')
+      expect(res.data.surveys[0].answers).toEqual([
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          image: null,
+          answer: 'other_answer'
+        }
+      ])
+      expect(res.data.surveys[0].didAnswer).toBe(false)
+      expect(res.data.surveys[0].date).toBe(now.toISOString())
+    })
+  })
 })
-// import { ApolloServer, gql } from 'apollo-server-express'
-// import { hash } from 'bcrypt'
-// import { sign } from 'jsonwebtoken'
-// import { Collection } from 'mongodb'
-
-// import { MongoHelper } from '@/infra/db'
-// import env from '@/main/config/env'
-
-// import { makeApolloServer } from './helpers'
-
-// const mockAccessToken = async (): Promise<string> => {
-//   const password = await hash('any_password', 12)
-//   const result = await accountCollection.insertOne({
-//     name: 'any_name',
-//     email: 'any_email@mail.com',
-//     password,
-//     role: 'admin'
-//   })
-//   const id = result.ops[0]._id
-//   const accessToken = sign({ id }, env.jwtSecret, {
-//     expiresIn: '1d'
-//   })
-//   await accountCollection.updateOne(
-//     { _id: id },
-//     {
-//       $set: {
-//         accessToken
-//       }
-//     }
-//   )
-//   return accessToken
-// }
-
-// let accountCollection: Collection
-// let surveyCollection: Collection
-// let apolloServer: ApolloServer
-// describe('Survey GraphQL', () => {
-//   beforeAll(async () => {
-//     apolloServer = makeApolloServer()
-//     await MongoHelper.connect(process.env.MONGO_URL)
-//   })
-
-//   beforeEach(async () => {
-//     accountCollection = await MongoHelper.getCollection('accounts')
-//     await accountCollection.deleteMany({})
-//     surveyCollection = await MongoHelper.getCollection('surveys')
-//     await surveyCollection.deleteMany({})
-//   })
-
-//   afterAll(async () => {
-//     await MongoHelper.disconnect()
-//   })
-
-//   describe('Surveys Query', () => {
-//     const surveysQuery = gql`
-//       query surveys {
-//         surveys {
-//           id
-//           question
-//           answers {
-//             image
-//             answer
-//           }
-//           date
-//           didAnswer
-//         }
-//       }
-//     `
-//     it('should return surveys', async () => {
-//       const accessToken = await mockAccessToken()
-//       const now = new Date()
-//       await surveyCollection.insertOne({
-//         question: 'any_question',
-//         answers: [
-//           {
-//             image: 'any_image',
-//             answer: 'any_answer'
-//           },
-//           {
-//             answer: 'other_answer'
-//           }
-//         ],
-//         date: now
-//       })
-//       const res = await apolloServer.executeOperation({
-//         query: surveysQuery,
-//         http: {
-//           headers: {}
-//         },
-//         extensions: {
-//           Headers: {
-//             'x-access-token': accessToken
-//           }
-//         }
-//       })
-//       expect(res.data.surveys.length).toBe(1)
-//       expect(res.data.surveys[0].id).toBeTruthy()
-//       expect(res.data.surveys[0].question).toBe('any_question')
-//       expect(res.data.surveys[0].answers).toEqual([
-//         {
-//           image: 'any_image',
-//           answer: 'any_answer'
-//         },
-//         {
-//           image: null,
-//           answer: 'other_answer'
-//         }
-//       ])
-//       expect(res.data.surveys[0].didAnswer).toBe(false)
-//       expect(res.data.surveys[0].date).toBe(now.toISOString())
-//     })
-//   })
-// })
